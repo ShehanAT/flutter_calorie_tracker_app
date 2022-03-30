a0ffbd001627d3f7c33c8a42fe1bbcf0014c8c6b
Nipun <shehanatuk@gmail.com>
2022-03-30 16:17:19 -0400

    removed code from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,144 +24,27 @@ class CalorieTrackerApp extends StatefulWidget {
 }
 
 class _CalorieTrackerAppState extends State<CalorieTrackerApp> {
-  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
-  late Widget homeWidget;
-  late bool signedIn;
-
   @override
-  void initState() {
-    super.initState();
-    checkFirstSeen();
-  }
+  Widget build(BuildContext context) {}
 
-  void checkFirstSeen() {
-    final bool _firstLaunch = true;
-
-    if (_firstLaunch) {
-      homeWidget = Homepage();
-    }
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider<DarkThemeProvider>(
-      create: (_) {
-        return themeChangeProvider;
-      },
-      child: Consumer<DarkThemeProvider>(
-        builder:
-            (BuildContext context, DarkThemeProvider value, Widget? child) {
-          return GestureDetector(
-              onTap: () => hideKeyboard(context),
-              child: MaterialApp(
-                  debugShowCheckedModeBanner: false,
-                  builder: (_, Widget? child) => ScrollConfiguration(
-                      behavior: MyBehavior(), child: child!),
-                  theme: themeChangeProvider.darkTheme ? darkTheme : lightTheme,
-                  home: homeWidget,
-                  onGenerateRoute: RoutePage.generateRoute));
-        },
-      ),
-    );
-  }
-
-  void hideKeyboard(BuildContext context) {
-    final FocusScopeNode currentFocus = FocusScope.of(context);
-    if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null) {
-      FocusManager.instance.primaryFocus!.unfocus();
-    }
-  }
+  void hideKeyboard(BuildContext context) {}
 }
 
 class Homepage extends StatefulWidget {
-  const Homepage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {}
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: FlatButton(
-          onPressed: () {
-            // Navigate back to homepage
-          },
-          child: const Text('Go Back!'),
-        ),
-      ),
-    );
-  }
-
-  @override
-  State<StatefulWidget> createState() {
-    return _Homepage();
-  }
+  State<StatefulWidget> createState() {}
 }
 
 class _Homepage extends State<Homepage> with SingleTickerProviderStateMixin {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  void onClickHistoryScreenButton(BuildContext context) {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => HistoryScreen()));
-  }
-
-  void onClickSettingsScreenButton(BuildContext context) {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => SettingsScreen()));
-  }
-
-  void onClickDayViewScreenButton(BuildContext context) {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => DayViewScreen()));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final ButtonStyle buttonStyle =
-        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
-
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Flutter Calorie Tracker App",
-            style: TextStyle(
-                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ),
-        body: new Column(
-          children: <Widget>[
-            new ListTile(
-                leading: const Icon(Icons.food_bank),
-                title: new Text("Welcome To Calorie Tracker App!",
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(fontWeight: FontWeight.bold))),
-            new ElevatedButton(
-                onPressed: () {
-                  onClickDayViewScreenButton(context);
-                },
-                child: Text("Day View Screen")),
-            new ElevatedButton(
-                onPressed: () {
-                  onClickHistoryScreenButton(context);
-                },
-                child: Text("History Screen")),
-            new ElevatedButton(
-                onPressed: () {
-                  onClickSettingsScreenButton(context);
-                },
-                child: Text("Settings Screen")),
-          ],
-        ));
-  }
+  Widget build(BuildContext context) {}
 }
 
 class MyBehavior extends ScrollBehavior {
   @override
   Widget buildViewportChrome(
-      BuildContext context, Widget child, AxisDirection axisDirection) {
-    return child;
-  }
+      BuildContext context, Widget child, AxisDirection axisDirection) {}
 }
